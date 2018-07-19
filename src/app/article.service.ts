ce7b6da832c5bb1b59b65706063adb1fe56c9c74
Yuriy Khuda <yuriy.khuda@vizexplorer.com>
2018-07-19 19:28:43 +0300

    added article service

---
+++ src/app/article.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpRequestor } from './common/http-requestor';
+import { Constants } from './common/constants';
+import { Articles } from './models/article';
+
+export let ARTICLES: Articles[] = [];
+
+@Injectable()
+export class ArticleService {
+  
+  constructor (public httpRequestor: HttpRequestor) { }
+
+  public getBussinessArticle(): Promise<any> {
+    return this.httpRequestor.getRequest(Constants.BussinessArticle).then( data => {
+      ARTICLES = data.articles;
+      console.log(ARTICLES);
+      return data.article;
+    });
+  }
+
+}
